365486410adbbfe41ddcc14291b185f4b1e88219
JabikU <enin.sacha@gmail.com>
2024-06-21 13:27:00 +0300

    Remake

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -9,62 +9,62 @@ export default class Blog extends Component {
                     <h2 className="text-center mt-5 mb-3"> </h2>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="http://cdn.motorpage.ru/Photos/800/7AE5.jpg" />
+                            <Card.Img variant="top" src="https://optim.tildacdn.com/tild3965-6333-4035-b231-616535333130/-/format/webp/DSCF7155-RAF.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://avatars.dzeninfra.ru/get-zen_doc/3412469/pub_5fd9cb3ab11a4f02b7565a32_5fd9cb73e7ae933e1e948f03/scale_1200" />
+                            <Card.Img variant="top" src="https://thumb.tildacdn.com/tild3861-3433-4635-b932-346565633130/-/format/webp/_20A0885-min.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://hdpic.club/photo/uploads/posts/2023-03/1679072404_hdpic-club-p-stil-avto-yaponskii-52.jpg" />
+                            <Card.Img variant="top" src="https://thumb.tildacdn.com/tild6230-6339-4537-b533-383765383135/-/format/webp/_20A0382.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://a.d-cd.net/BiAAAgPuxOA-1920.jpg" />
+                            <Card.Img variant="top" src="https://thumb.tildacdn.com/tild3766-3637-4239-b466-643665646131/-/format/webp/_20A0644.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://avatars.dzeninfra.ru/get-zen_doc/9429031/pub_640ada85508a173b5b300968_640add542641cf5c1797278e/scale_1200" />
+                            <Card.Img variant="top" src="https://thumb.tildacdn.com/tild3930-3162-4432-b334-666130356261/-/format/webp/FAN_6863.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://i.pinimg.com/originals/59/96/36/599636f7e8ed7a5588ad30a6a80517ac.jpg" />
+                            <Card.Img variant="top" src="https://thumb.tildacdn.com/tild6364-3838-4463-b338-643233333263/-/format/webp/FAN_5406.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://a.d-cd.net/ead0b3es-1920.jpg" />
+                            <Card.Img variant="top" src="https://optim.tildacdn.com/tild3362-3666-4231-b962-356134646630/-/format/webp/_20A3448.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://a.d-cd.net/1ad0b3es-1920.jpg" />
+                            <Card.Img variant="top" src="https://optim.tildacdn.com/tild3563-3765-4233-a666-363730336536/-/format/webp/_20A6293-min.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://a.d-cd.net/5eae286s-1920.jpg" />
+                            <Card.Img variant="top" src="https://optim.tildacdn.com/tild6161-3635-4463-b130-393932653766/-/format/webp/_20A5968-min.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://img.razrisyika.ru/kart/48/1200/190012-jdm-mashiny-8.jpg" />
+                            <Card.Img variant="top" src="https://thumb.tildacdn.com/tild3038-6263-4635-a136-333232616433/-/format/webp/DSCF4454-RAF.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://klike.net/uploads/posts/2023-04/1680841513_3-34.jpg" />
+                            <Card.Img variant="top" src="https://thumb.tildacdn.com/tild3161-6439-4561-b637-343061346363/-/format/webp/DSCF4381-RAF.jpg" />
                         </Card>
                     </Col>
                     <Col md={3}>
                         <Card className="mb-3">
-                            <Card.Img variant="top" src="https://a.d-cd.net/628e4cs-960.jpg" />
+                            <Card.Img variant="top" src="https://thumb.tildacdn.com/tild3861-3539-4138-b765-363665653061/-/format/webp/0E7A7136-min.jpg" />
                         </Card>
                     </Col>
                 </Row>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -15,98 +15,100 @@ export default class Blog extends Component {
             <Carousel.Item>
               <img
                 className="d-block w-100"
-                src="https://hdpic.club/uploads/posts/2021-12/1640182648_25-hdpic-club-p-mazda-rx7-37.jpg"
+                src="https://img.banket.ru/restaurants/restaurant_1469/gallery/file3ee775fa14eff9bd895c075bc9d13372.jpg"
                 alt="First slide"
               />
               <Carousel.Caption>
-                <h3>UNDERGROUND</h3>
-                <p>8-9 сентября, Севкабель порт</p>
+                <h3>BASSCL</h3>
+                <p>Севкабель порт</p>
               </Carousel.Caption>
             </Carousel.Item>
             {/* след карусель если надо */}
             <Carousel.Item>
               <img
                 className="d-block w-100"
-                src="https://c.wallhere.com/photos/0e/71/1920x1080_px_Green_Cars_JDM_Nissan_Nissan_240SX-1096098.jpg!d"
+                src="https://mc-guide.ru/wp-content/uploads/2022/07/glav.jpeg"
                 alt="First slide"
               />
               <Carousel.Caption>
-                <h3>UNDERGROUND</h3>
-                <p>8-9 сентября, Севкабель порт</p>
+                <h3>BASSCL</h3>
+                <p>Севкабель порт</p>
           <hr/>
               </Carousel.Caption>
                   </Carousel.Item>
                   <Carousel.Item>
                       <img
                           className="d-block w-100"
-                          src="https://img.goodfon.ru/original/1600x900/e/b5/christer-stormark-by-christer-stormark-r32-skyline-white-edi.jpg"
+                          src="https://www.gdebar.ru/data/app/bar/img/gallery/10800/251430.webp"
                           alt="First slide"
                       />
                       <Carousel.Caption>
-                          <h3>UNDERGROUND</h3>
-                <p>8-9 сентября, Севкабель порт</p>
+                          <h3>BASSCL</h3>
+                <p>Севкабель порт</p>
                           <hr />
                       </Carousel.Caption>
                   </Carousel.Item>
                   <Carousel.Item>
                       <img
                           className="d-block w-100"
-                          src="https://a-static.besthdwallpaper.com/red-toyota-supra-mk4-with-bady-kit-applied-wallpaper-1600x900-92502_47.jpg"
+                          src="https://eventmoskva.ru/bitrix/templates/EventMoskva/img/places/atmosphere/thumbs/4.jpg"
                           alt="First slide"
                       />
                       <Carousel.Caption>
-                          <h3>UNDERGROUND</h3>
-                <p>8-9 сентября, Севкабель порт</p>
+                          <h3>BASSCL</h3>
+                <p>Севкабель порт</p>
                           <hr />
                       </Carousel.Caption>
                   </Carousel.Item>
               </Carousel>
 
           <Container>
-            <h2 className="text-center mt-5 mb-3">О мероприятии</h2>
+            <h2 className="text-center mt-5 mb-3">О клубе</h2>
               <Col md={12}>
                 <Card>
                   <Card.Body>
-                    <Card.Title>UNDERGROUND -</Card.Title>
-                    <Card.Text> это мероприятие, на котором представлены автомобили с заниженным клиренсом и экстремальными параметрами углов постановки колёс. Экспозиция проходит в        Севкабель Порт в Санкт-Петербурге. На выставке можно увидеть автомобили разных марок и моделей, выполненные в стиле стенс.</Card.Text>
+                    <Card.Title>BASSCL</Card.Title>
+                    <Card.Text> Что же такое BASSCL?
+
+2500 кв.м площади, где можно проводить мероприятия любого формата: от камерных закрытых событий до масштабных концертов популярных артистов. Два этажа, которые можно зонировать абсолютно по разному: разделить на фан-зону и танцевальный партер, разместить сидячие места, добавить кресла или танцпол на балконе.</Card.Text>
                     <Link to="/blog">Подробнее</Link> 
                   </Card.Body>
                 </Card>
               </Col>
               {/* след товар */}
-            <h2 className="text-center mt-5 mb-3">Галлерея</h2>
+            <h2 className="text-center mt-5 mb-3">Афиша</h2>
             <Row>
               <Col md={3}>
                 <Card>
-                  <Card.Img variant="top" src="https://avatars.dzeninfra.ru/get-zen_doc/3690562/pub_5f32e926f8858d597854a480_5f32ea08a0afa33797cbb37a/scale_1200" />
+                  <Card.Img variant="top" src="https://images.radario.ru/images/afficheevent/cd68aae0fee144e5b5cb39987e621082.jpg" />
                   <CardBody>
-                    <Link to="/gallery">Подробнее</Link> 
+                    <Link to="/gallery">Купить билет</Link> 
                   </CardBody>
                 </Card>
               </Col>
               {/* след акции */}
               <Col md={3}>
                 <Card>
-                  <Card.Img variant="top" src="https://kartinki.pics/pics/uploads/posts/2022-08/thumbs/1661466317_33-kartinkin-net-p-yaponskie-mashini-dlya-drifta-oboi-krasivo-42.jpg" />
+                  <Card.Img variant="top" src="https://sun9-18.userapi.com/impf/c855136/v855136400/150778/DzF8z5H6HKM.jpg?size=604x403&quality=96&sign=3133a40beb80c517f22f07c6dcc940fb&type=album" />
                   <CardBody>
-                    <Link to="/gallery">Подробнее</Link> 
+                    <Link to="/gallery">Купить билет</Link> 
                   </CardBody>
                 </Card>
               </Col>
               {/*  след акции */}
               <Col md={3}>
                 <Card>
-                  <Card.Img variant="top" src="https://klike.net/uploads/posts/2023-01/1674994328_4-57.jpg" />
+                  <Card.Img variant="top" src="https://sun9-20.userapi.com/impg/AC2XfyLc7Zsg1VakjHCwpnWped_pvpEN0XszCg/JKgSsu90wZg.jpg?size=1920x1080&quality=95&sign=d5dd1d4f3cfc0e1a99db53f173387a3d&c_uniq_tag=2l91vGOqj0gTdUC6SWezXN-vGmJDObYpn2DF65bYkhc&type=album" />
                   <CardBody>
-                    <Link to="/gallery">Подробнее</Link> 
+                    <Link to="/gallery">Купить билет</Link> 
                   </CardBody>
                 </Card>
               </Col>
               <Col md={3}>
                 <Card>
-                  <Card.Img variant="top" src="https://happylove.top/uploads/posts/2023-06/thumbs/1685606890_happylove-top-p-starii-tokio-vkontakte-48.jpg" />
+                  <Card.Img variant="top" src="https://avatars.dzeninfra.ru/get-zen_doc/271828/pub_659dac1d89c0fd69095f5909_659dac48865a591d8bc9b667/scale_1200" />
                   <CardBody>
-                    <Link to="/gallery">Подробнее</Link> 
+                    <Link to="/gallery">Купить билет</Link> 
                   </CardBody>
                 </Card>
               </Col>
